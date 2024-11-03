75429ed337afc8fc518f7621eece0c51259fbfe1
LeeTaeYoung <xodud5080@naver.com>
2024-11-03 17:11:56 +0900

    회원가입 수정 및 fetch 구현중

--- src/signup/js/signup_info.js
+++ src/signup/js/signup_info.js
@@ -9,67 +9,143 @@ const Signup_info = () => {
 
     const navigate = useNavigate();
 
+    // 이메일 인증
+    let [emailValue, setEmailValue] = useState('');
     let [emailAuthenticationBtn, setEmailAuthenticationBtn] = useState(false);
-    
-    let [emailContent, setEmailContent] = useState('');
-    let [emailContentLength, setEmailContentLength] = useState(0);
-    let [usernameContent, setUsernameContent] = useState('');
-    let [usernameContentLength, setUsernameContentLength] = useState(0);
-    let [passwordContent, setPasswordContent] = useState('');
-    let [passwordContentLength, setPasswordContentLength] = useState(0);
-    let [code, setCode] = useState('');
-    let [codeLength, setCodeLength] = useState(0);
 
     const handleEmailChange = (e) => {
-        setEmailContent(e.target.value);
-        setEmailContentLength(e.target.value.length);
-    }
+        setEmailValue(e.target.value);
+    };
+
+    const fetchEmailAuthentication = async () => {
+
+        console.log("이메일 : ", emailValue);
+
+        setEmailAuthenticationBtn(true);
+
+    };
+
+    // 코드 인증
+    let [codeValue, setCodeValue] = useState('');
+    let [codeAuthenticationBtn, setCodeAuthenticationBtn] = useState(false);
+    
     const handleCodeChange = (e) => {
-        setCode(e.target.value);
-        setCodeLength(e.target.value.length);
-    }
+        setCodeValue(e.target.value);
+    };
+
+    const fetchCodeAuthentication = async () => {
+
+        console.log("코드 : ", codeValue);
+
+        setCodeAuthenticationBtn(true);
+
+    };
+
+    // 유저네임 중복 체크
+    let [usernameValue, setUsernameValue] = useState('');
+    let [usernameDuplicationBtn, setUsernameDuplicationBtn] = useState(false)
+ 
     const handleUsernameChange = (e) => {
-        setUsernameContent(e.target.value);
-        setUsernameContentLength(e.target.value.length);
+        setUsernameValue(e.target.value);
     };
+
+    const fetchUsernameDuplication = async () => {
+
+        console.log("유저네임 : ", usernameValue);
+
+        setUsernameDuplicationBtn(true);
+
+    };
+
+    // 버튼들 확인
+    useEffect(() => {
+        if (emailAuthenticationBtn) {
+            console.log("이메일 인증 버튼 활성화");
+        }
+        if (codeAuthenticationBtn) {
+            console.log("코드 인증 버튼 활성화");
+        }
+        if (usernameDuplicationBtn) {
+            console.log("유저네임 중복 버튼 활성화");
+        }
+    }, [emailAuthenticationBtn, codeAuthenticationBtn, usernameDuplicationBtn]);
+
+
+    // 패스워드 확인
+    let [passwordValue, setPasswordValue] = useState('');
+
     const handlePasswordChange = (e) => {
-        setPasswordContent(e.target.value);
-        setPasswordContentLength(e.target.value.length);
+        setPasswordValue(e.target.value);
     };
 
-    let [finalEmailBtn, setFinalEmailBtn] = useState(false);
-    let [finalUsernamBtn, setFinalUsernameBtn] = useState(false);
-    let [finalPassword, setFinalPassword] = useState(false);
-    let [finalSignupBtn, setFinalSignupBtn] = useState(false);
+    
+    // 회원가입 버튼 활성화
+    let [SignupBtn, setSignupBtn] = useState(false);
 
-    /* 비밀번호 길이 체크 */
     useEffect(() => {
-        if (passwordContentLength >= 8) {
-            setFinalPassword(true);
+        if(codeAuthenticationBtn && usernameDuplicationBtn && (passwordValue.length >= 8)){
+            setSignupBtn(true);
         }else{
-            setFinalPassword(false);
+            setSignupBtn(false);
         }
-    }, [passwordContentLength, finalPassword])
+    }, [codeAuthenticationBtn, usernameDuplicationBtn, passwordValue, SignupBtn]);
 
-    /* 최종 회원가입 */
-    useEffect(() => {
-        if(finalEmailBtn && finalPassword ){
-            setFinalSignupBtn(true);
-        }else{
-            setFinalSignupBtn(false);
-        }
-    }, [finalEmailBtn, finalPassword, finalSignupBtn])
 
-    /* 테스트용 */
-    useEffect(() => {
-        console.log("이메일 : ", emailContent);
-        console.log("코드 : ", code);
-        console.log("코드 길이 : ", codeLength);
-        console.log("유저네임 : ", usernameContent);
-        console.log("유저네임 길이 : ", usernameContentLength);
-        console.log("비밀번호 : ", passwordContent);
-        console.log("비밀번호 길이 : ", passwordContentLength)
-    }, [emailContent, code, usernameContent, passwordContent]);
+    // 최종 회원가입
+    const fetchSignup = async () => {
+        console.log("비밀번호 : ", passwordValue);
+        console.log("최종 회원가입");
+
+    }
+
+    // let [data, setData] = useState(null); // 데이터를 저장할 상태
+    // let [loading, setLoading] = useState(true); // 로딩 상태
+    // let [error, setError] = useState(null); // 에러 상태
+
+    // const fetchData = async () => {
+    //     console.log("최종 이메일 : ", );
+    //     console.log("최종 닉네임 : ", );
+    //     console.log("최종 비밀번호 : ", );
+
+    //     setLoading(true); // 로딩 시작
+    //     try {
+    //         const response = await fetch('http://localhost:8080/login', {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({
+    //                 email : "user@gmail.com",
+    //                 password : "password",
+    //                 nickname : "nickname",
+    //             })
+    //         });
+
+    //         // status 확인
+    //         console.log("status 확인 : ", response.status);
+
+    //         if (response.status === 200) {
+    //             // 성공적인 요청인 경우 (status 200)
+    //             const result = await response.json();
+    //             setData(result);
+    //         } else if (response.status === 401) {
+    //             // 인증 실패 등의 상황 (status 401)
+    //             setError("인증에 실패했습니다. 이메일과 비밀번호를 확인하세요.");
+    //         } else {
+    //             // 그 외의 에러
+    //             throw new Error(`Error: ${response.status}`);
+    //         }
+            
+    //     } catch (error) {
+    //         setError(error.message);
+    //     } finally {
+    //         setLoading(false); // 로딩 종료
+    //     }
+    // };
+    // useEffect(() => {
+
+    // }, []);
+
 
     return(
         <body className='signup_container'>
@@ -78,21 +154,21 @@ const Signup_info = () => {
                     {
                         emailAuthenticationBtn ? 
                         <div className='media_signup_input_email_container'>
-                            <input readOnly className='signup_input-email_hold_content' placeholder='Email' value={emailContent} onChange={handleEmailChange}></input>
-                            <input className='signup_input_verification_code_content' placeholder='Verification Code' value={code} onChange={handleCodeChange}></input>
+                            <input readOnly className='signup_input-email_hold_content' placeholder='Email' value={emailValue}></input>
+                            <input className='signup_input_verification_code_content' placeholder='Verification Code' onChange={handleCodeChange}></input>
                             {
-                                codeLength >= 1 ?
-                                <button onClick={() => setFinalEmailBtn(true)} className='signup_verification_code_btn'> Code Authentication</button>
+                                codeValue.length >= 1 ?
+                                <button onClick={() => fetchCodeAuthentication()} className='signup_verification_code_btn'> Code Authentication</button>
                                 :
                                 <button className='signup_verification_code_none_btn'> Code Authentication</button>
                             }
                         </div>
                         :
                         <div className='media_signup_input_email_container'>
-                            <input className='signup_input-email_content' placeholder='Email' value={emailContent} onChange={handleEmailChange}></input>
+                            <input className='signup_input-email_content' placeholder='Email' onChange={handleEmailChange}></input>
                             {
-                                emailContentLength >= 1 ?
-                                <button onClick={() => setEmailAuthenticationBtn(true)} className='signup_email_authentication_btn'>Email Authentication</button>
+                                emailValue.length >= 1 ?
+                                <button onClick={() => fetchEmailAuthentication()} className='signup_email_authentication_btn'>Email Authentication</button>
                                 :
                                 <button className='signup_email_authentication_none_btn'>Email Authentication</button>
                             }
@@ -102,9 +178,9 @@ const Signup_info = () => {
 
                 <div className='signup_input-username_container'>
                     <div className='media_signup_input-username_container'>
-                        <input className='signup_input-username_content' maxLength={8} placeholder='Username (min 3 characters & max 8 characters)' value={usernameContent} onChange={handleUsernameChange}></input>
-                        {usernameContentLength >= 3 ? 
-                            <button onClick={() => setFinalUsernameBtn(true)} className='signup_username_duplication_btn'>Duplication Check</button>
+                        <input className='signup_input-username_content' maxLength={8} placeholder='Username (min 3 characters & max 8 characters)' onChange={handleUsernameChange}></input>
+                        {usernameValue.length >= 3 ? 
+                            <button onClick={() => fetchUsernameDuplication()} className='signup_username_duplication_btn'>Duplication Check</button>
                             :
                             <button className='signup_username_duplication_none_btn'>Duplication Check</button>
                         }
@@ -112,13 +188,13 @@ const Signup_info = () => {
                 </div>  
 
                 <div className='signup_input-password_container'>
-                    <input className='signup_input-password_content' placeholder='Password (min 8 characters)' value={passwordContent} onChange={handlePasswordChange}></input>
+                    <input className='signup_input-password_content' placeholder='Password (min 8 characters)' onChange={handlePasswordChange}></input>
                 </div> 
 
                 <div className='signup_btn_container'>
                     {
-                        finalSignupBtn ? 
-                        <button className='signup_btn'>
+                        SignupBtn ? 
+                        <button onClick={() => fetchSignup()} className='signup_btn'>
                             Sign up
                         </button>
                         :
